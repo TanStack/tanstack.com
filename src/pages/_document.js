da8e6fe1b81dd2fb4fc1832e793588c3ac4be342
Tanner Linsley <tannerlinsley@gmail.com>
2020-12-28 15:39:35 -0700

    feat: add GA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-JMT1Z50SPS"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JMT1Z50SPS');
+          `,
+            }}
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />

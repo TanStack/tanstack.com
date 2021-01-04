e767bf5364be0018a2cf3df379d3007d25de29c8
Tanner Linsley <tannerlinsley@gmail.com>
2021-01-03 17:26:13 -0700

    feat: github-sponsors-webhook

---
+++ src/pages/api/github-sponsors-webhook.js
@@ -0,0 +1,32 @@
+// import { middleware } from '../../server/middleware'
+
+async function verifySecret(req, res) {
+  const sig = req.headers['x-hub-signature'] || ''
+  const hmac = crypto.createHmac(
+    'sha1',
+    process.env.GITHUB_SPONSORS_WEBHOOK_SECRET
+  )
+  const digest = Buffer.from(
+    'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex'),
+    'utf8'
+  )
+  const checksum = Buffer.from(sig, 'utf8')
+  if (
+    checksum.length !== digest.length ||
+    !crypto.timingSafeEqual(digest, checksum)
+  ) {
+    throw new Error(
+      `Request body digest (${digest}) did not match x-hub-signature (${checksum})`
+    )
+  }
+}
+
+export default async function handler(req, res) {
+  // Run the middleware
+  await verifySecret(req, res)
+
+  res.status(200)
+  // .json({
+  //   message: Date.now(),
+  // })
+}

81e402668655c5fc37eec98c3c4e73ee0dc0f639
Jacob Ebey <jacob.ebey@live.com>
2021-12-12 20:12:52 -0800

    feat: render to a stream to allow flushing headers

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,4 @@
-import { renderToString } from 'react-dom/server'
+import { PassThrough } from 'stream'
 import { RemixServer } from 'remix'
 import type { EntryContext } from 'remix'
 import { initStyles, renderWithStyles } from './utils/styleContext'
@@ -13,12 +13,19 @@ export default function handleRequest(
 ) {
   const [markup, hashPath] = renderWithStyles(
     <RemixServer context={remixContext} url={request.url} />
-  )
+  );
+
+  let body = new PassThrough()
+  setImmediate(() => {
+    body.push('<!DOCTYPE html>')
+    body.push(markup)
+    body.push(null)
+  })
 
   responseHeaders.set('Content-Type', 'text/html')
   responseHeaders.append('Link', `<${hashPath}>; rel=preload; as=style`)
 
-  return new Response('<!DOCTYPE html>' + markup, {
+  return new Response(body as any, {
     status: responseStatusCode,
     headers: responseHeaders,
   })

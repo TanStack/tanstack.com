fec2d18e777c47a6ea063af0c1942eba1ea39071
Tanner Linsley <tannerlinsley@gmail.com>
2022-05-23 21:41:11 -0600

    More vercel prep

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   appDirectory: 'app',
   browserBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: '.vercel/output/functions/index.func/_build',
+  serverBuildDirectory: 'vercel/output/functions/index.func/_build',
   devServerPort: 8002,
   serverDependenciesToBundle: [
     /^rehype.*/,

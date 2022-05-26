9c4fedfa8b202a21b21c547ef8fc8d3ac27795e8
Tanner Linsley <tannerlinsley@gmail.com>
2022-05-26 07:52:48 -0600

    Fix blog linkages

--- app/utils/docCache.server.ts
+++ app/utils/docCache.server.ts
@@ -64,6 +64,8 @@ export async function fetchRepoFile(
     const href = new URL(`/${filePath}`, 'https://raw.githubusercontent.com/')
       .href
 
+    console.log('Fetching:', href)
+
     let response = await fetch(href, {
       headers: { 'User-Agent': `docs:${owner}/${repo}` },
     })

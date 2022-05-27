f1a9b8ef01a5d8c46ba01e10e0b15cb7fedfd9ae
Tanner Linsley <tannerlinsley@gmail.com>
2022-05-27 10:50:15 -0600

    No logs

--- app/routes/table/$.tsx
+++ app/routes/table/$.tsx
@@ -2,7 +2,6 @@ import { LoaderFunction } from 'remix'
 import { handleRedirects } from '~/redirects'
 
 export const loader: LoaderFunction = (context) => {
-  console.log('hello', context.request.url)
   handleRedirects(context)
 
   return new Response('OK')

b0e4de46f90165194e64d909fd85ed95f4999e93
Tanner Linsley <tannerlinsley@gmail.com>
2022-06-02 17:56:32 -0600

    Update index.tsx

--- app/routes/virtual/v3/index.tsx
+++ app/routes/virtual/v3/index.tsx
@@ -15,6 +15,7 @@ import { Carbon } from '~/components/Carbon'
 import { Footer } from '~/components/Footer'
 import { IoIosBody } from 'react-icons/io'
 import SponsorPack from '~/components/SponsorPack'
+import { capitalize } from '~/utils/utils'
 
 export const gradientText =
   'inline-block text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-blue-600'
@@ -355,25 +356,42 @@ export default function ReactTableRoute() {
         </div>
       </div>
 
-      <div className="bg-black body-font">
-        <iframe
-          key={framework}
-          src={`https://codesandbox.io/embed/github/tanstack/virtual/tree/${v3branch}/examples/${framework}/dynamic?autoresize=1&fontsize=16&theme=dark`}
-          title="tannerlinsley/react-table: dynamic"
-          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-          className="shadow-2xl"
-          loading="lazy"
-          style={{
-            width: '100%',
-            height: '80vh',
-            border: '0',
-            borderRadius: 8,
-            overflow: 'hidden',
-            position: 'static',
-            zIndex: 0,
-          }}
-        ></iframe>
-      </div>
+      {['vue', 'solid', 'svelte'].includes(framework) ? (
+        <div className="px-2">
+          <div className="p-8 text-center text-lg w-full max-w-screen-lg mx-auto bg-black text-white rounded-xl">
+            Looking for the <strong>@tanstack/{framework}-virtual</strong>{' '}
+            example? We could use your help to build the{' '}
+            <strong>@tanstack/{framework}-virtual</strong> adapter! Join the{' '}
+            <a
+              href="https://tlinz.com/discord"
+              className="text-teal-500 font-bold"
+            >
+              TanStack Discord Server
+            </a>{' '}
+            and let's get to work!
+          </div>
+        </div>
+      ) : (
+        <div className="bg-black body-font">
+          <iframe
+            key={framework}
+            src={`https://codesandbox.io/embed/github/tanstack/virtual/tree/${v3branch}/examples/${framework}/dynamic?autoresize=1&fontsize=16&theme=dark`}
+            title="tannerlinsley/react-table: dynamic"
+            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            className="shadow-2xl"
+            loading="lazy"
+            style={{
+              width: '100%',
+              height: '80vh',
+              border: '0',
+              borderRadius: 8,
+              overflow: 'hidden',
+              position: 'static',
+              zIndex: 0,
+            }}
+          ></iframe>
+        </div>
+      )}
 
       <div className="flex flex-col gap-4 items-center">
         <div className="font-extrabold text-xl lg:text-2xl">

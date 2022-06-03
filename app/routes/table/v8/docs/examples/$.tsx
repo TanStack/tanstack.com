c2014a4aa28db0f61240de73b86477106acab691
Tanner Linsley <tannerlinsley@gmail.com>
2022-06-03 11:16:46 -0600

    fixes

--- app/routes/table/v8/docs/examples/$.tsx
+++ app/routes/table/v8/docs/examples/$.tsx
@@ -41,14 +41,16 @@ export default function RouteReactTableDocs() {
           {capitalize(kind)} Example: {slugToTitle(name)}
         </DocTitle>
       </div>
-      <iframe
-        src={`https://codesandbox.io/embed/github/tanstack/table/tree/${v8branch}/examples/${examplePath}?autoresize=1&fontsize=14&theme=${
-          isDark ? 'dark' : 'light'
-        }`}
-        title={`tanstack/table: ${examplePath}`}
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        className="flex-1 w-full overflow-hidden lg:ml-6 lg:rounded-l-2xl shadow-xl shadow-gray-700/20 bg-white dark:bg-black"
-      />
+      <div className="flex-1 lg:ml-6 flex flex-col min-h-0">
+        <iframe
+          src={`https://codesandbox.io/embed/github/tanstack/table/tree/${v8branch}/examples/${examplePath}?autoresize=1&fontsize=14&theme=${
+            isDark ? 'dark' : 'light'
+          }`}
+          title={`tanstack/table: ${examplePath}`}
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+          className="flex-1 w-full overflow-hidden lg:rounded-l-2xl shadow-xl shadow-gray-700/20 bg-white dark:bg-black"
+        />
+      </div>
       <div className="h-16 lg:mt-2" />
     </div>
   )

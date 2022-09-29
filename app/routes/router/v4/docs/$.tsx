df900bf144f9bb593e9e902100aca765db523bb7
Tobias <t@tobiasjordans.de>
2022-09-29 11:54:39 +0200

    Docs: Use "Router Docs" for the router docs
    Fixes https://github.com/TanStack/router/issues/330

--- app/routes/router/v4/docs/$.tsx
+++ app/routes/router/v4/docs/$.tsx
@@ -54,7 +54,7 @@ export const loader: LoaderFunction = async (context) => {
 
 export let meta: MetaFunction = ({ data }) => {
   return seo({
-    title: `${data?.title ?? 'Docs'} | TanStack Table Docs`,
+    title: `${data?.title ?? 'Docs'} | TanStack Router Docs`,
     description: data?.description,
   })
 }

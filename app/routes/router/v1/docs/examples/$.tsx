a80a0f78c2ca9ce95f2560729b63866f10e93ef1
Judicael <46365844+judicaelandria@users.noreply.github.com>
2023-01-27 10:40:42 +0300

    fix(title): react router title

--- app/routes/router/v1/docs/examples/$.tsx
+++ app/routes/router/v1/docs/examples/$.tsx
@@ -1,41 +1,41 @@
-import * as React from 'react'
-import type { LoaderFunction, MetaFunction } from '@remix-run/node'
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { DocTitle } from '~/components/DocTitle'
-import { v1branch } from '~/routes/router/v1'
-import { seo } from '~/utils/seo'
-import { capitalize, slugToTitle } from '~/utils/utils'
+import * as React from "react";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { DocTitle } from "~/components/DocTitle";
+import { v1branch } from "~/routes/router/v1";
+import { seo } from "~/utils/seo";
+import { capitalize, slugToTitle } from "~/utils/utils";
 
 export const loader: LoaderFunction = async (context) => {
-  const { '*': examplePath } = context.params
-  const [kind, _name] = (examplePath ?? '').split('/')
-  const [name, search] = _name.split('?')
+  const { "*": examplePath } = context.params;
+  const [kind, _name] = (examplePath ?? "").split("/");
+  const [name, search] = _name.split("?");
 
-  return json({ kind, name, search: search ?? '' })
-}
+  return json({ kind, name, search: search ?? "" });
+};
 
 export let meta: MetaFunction = ({ data }) => {
   return seo({
-    title: `${capitalize(data.kind)} Virtual ${slugToTitle(
+    title: `${capitalize(data.kind)} Router ${slugToTitle(
       data.name
-    )} Example | TanStack Virtual Docs`,
+    )} Example | TanStack Router Docs`,
     description: `An example showing how to implement ${slugToTitle(
       data.name
-    )} in ${capitalize(data.kind)} Virtual`,
-  })
-}
+    )} in ${capitalize(data.kind)} Router`,
+  });
+};
 
 export default function RouteReactTableDocs() {
-  const { kind, name, search } = useLoaderData()
+  const { kind, name, search } = useLoaderData();
 
-  const examplePath = [kind, name].join('/')
+  const examplePath = [kind, name].join("/");
 
-  const [isDark, setIsDark] = React.useState(true)
+  const [isDark, setIsDark] = React.useState(true);
 
   React.useEffect(() => {
-    setIsDark(window.matchMedia?.(`(prefers-color-scheme: dark)`).matches)
-  }, [])
+    setIsDark(window.matchMedia?.(`(prefers-color-scheme: dark)`).matches);
+  }, []);
 
   return (
     <div className="flex-1 flex flex-col min-h-0 overflow-auto">
@@ -47,7 +47,7 @@ export default function RouteReactTableDocs() {
       <div className="flex-1 lg:ml-6 flex flex-col min-h-0">
         <iframe
           src={`https://stackblitz.com/github/tanstack/router/tree/${v1branch}/examples/${examplePath}?${search}embed=1&theme=${
-            isDark ? 'dark' : 'light'
+            isDark ? "dark" : "light"
           }`}
           title={`tanstack/router: ${examplePath}`}
           sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
@@ -56,5 +56,5 @@ export default function RouteReactTableDocs() {
       </div>
       <div className="lg:h-16 lg:mt-2" />
     </div>
-  )
+  );
 }

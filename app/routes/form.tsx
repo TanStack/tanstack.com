07f8b2095ec33aa48c63649dcb88cbb2055a8b43
Tanner Linsley <tannerlinsley@gmail.com>
2023-04-28 08:43:43 -0600

    fix: tanstack form

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -69,7 +69,7 @@ export const createLogo = (version?: string) => (
     <Link to="/" className="font-light">
       TanStack
     </Link>
-    <Link to="../.." className={`font-bold`}>
+    <Link to="../../" className={`font-bold`}>
       <span className={`${gradientText}`}>Form</span>{' '}
       <span className="text-sm align-super">
         {version === 'latest' ? latestVersion : version}

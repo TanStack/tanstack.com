067fb4727d82c3254a3a4f9c600975d6ccbe722e
Tanner Linsley <tannerlinsley@gmail.com>
2023-08-02 15:33:13 -0600

    small fixes

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -1,14 +1,13 @@
 import type { FC, HTMLAttributes, ReactElement } from 'react'
-import { Children } from 'react'
 import invariant from 'tiny-invariant'
 import type { Language } from 'prism-react-renderer'
 import { Highlight, Prism } from 'prism-react-renderer'
 
 // @ts-ignore Alias markup as vue highlight
-Prism.languages.vue = Prism.languages.markup;
+Prism.languages.vue = Prism.languages.markup
 
 // @ts-ignore Alias markup as svelte highlight
-Prism.languages.svelte = Prism.languages.markup;
+Prism.languages.svelte = Prism.languages.markup
 
 function getLanguageFromClassName(className: string) {
   const match = className.match(/language-(\w+)/)

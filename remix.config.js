7e1693e7fc96ae6daa0884fdf3383d680cb29973
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-11-05 07:45:09 +1100

    Update remix and react deps

--- remix.config.js
+++ remix.config.js
@@ -11,12 +11,4 @@ module.exports = {
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
   serverModuleFormat: 'cjs',
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }

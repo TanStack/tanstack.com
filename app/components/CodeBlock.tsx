07795f05d7d39f3bd31af98ce0c7a646e20b0942
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-11-05 15:04:41 +1100

    Fix CodeBlock borders

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -9,25 +9,21 @@ function getLanguageFromChildren(children: any): string {
 export const CodeBlock: FC<HTMLAttributes<HTMLPreElement>> = (props) => {
   const { children, className, style } = props
   invariant(!!children, 'children is required')
-  const userLang = getLanguageFromChildren(children)
+  const lang = getLanguageFromChildren(children)
   return (
-    <div className="w-full max-w-full">
-      <div className="relative not-prose">
-        <div
-          className="absolute bg-white text-sm z-10 border border-gray-300 px-2 rounded-md -top-3 right-2
+    <div className="relative not-prose w-full max-w-full">
+      <div
+        className="absolute bg-white text-sm z-10 border border-gray-300 px-2 rounded-md -top-3 right-2
             dark:bg-gray-600 dark:border-0"
-        >
-          {userLang}
-        </div>
-        <div
-          className="rounded-md font-normal w-full border border-gray-200
-              dark:border-0"
-        >
-          <pre className={`${className} border-none m-0`} style={style}>
-            {children}
-          </pre>
-        </div>
+      >
+        {lang}
       </div>
+      <pre
+        className={`${className} dark:border-0 m-0 rounded-md w-full border border-gray-200`}
+        style={style}
+      >
+        {children}
+      </pre>
     </div>
   )
 }

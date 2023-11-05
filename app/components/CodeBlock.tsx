a244f4cf420a49126b95860781f7e276a1d009b6
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-11-05 18:28:30 +1100

    Don't render lang if absent

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -1,9 +1,9 @@
 import type { FC, HTMLAttributes } from 'react'
 import invariant from 'tiny-invariant'
 
-function getLanguageFromChildren(children: any): string {
+function getLanguageFromChildren(children: any): string | undefined {
   const language = children[0]?.props?.children
-  return language ? language : ''
+  return language ? language : undefined
 }
 
 export const CodeBlock: FC<HTMLAttributes<HTMLPreElement>> = (props) => {
@@ -12,12 +12,14 @@ export const CodeBlock: FC<HTMLAttributes<HTMLPreElement>> = (props) => {
   const lang = getLanguageFromChildren(children)
   return (
     <div className="relative not-prose w-full max-w-full">
-      <div
-        className="absolute bg-white text-sm z-10 border border-gray-300 px-2 rounded-md -top-3 right-2
+      {lang && (
+        <div
+          className="absolute bg-white text-sm z-10 border border-gray-300 px-2 rounded-md -top-3 right-2
             dark:bg-gray-600 dark:border-0"
-      >
-        {lang}
-      </div>
+        >
+          {lang}
+        </div>
+      )}
       <pre
         className={`${className} dark:border-0 m-0 rounded-md w-full border border-gray-200`}
         style={style}

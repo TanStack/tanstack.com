2c52ab097be92a48b9f699107b17b749f2cd0285
Tanner Linsley <tannerlinsley@gmail.com>
2023-12-20 11:56:56 -0700

    updates

--- app/components/Scarf.tsx
+++ app/components/Scarf.tsx
@@ -8,7 +8,7 @@ export function Scarf({ id }: { id: string }) {
       key={location.key}
       alt="scarf analytics"
       referrerPolicy="no-referrer-when-downgrade"
-      src={`https://static.scarf.sh/a.png?x-pxid=${id}?key=${location.key}`}
+      src={`https://static.scarf.sh/a.png?x-pxid=${id}&key=${location.key}`}
     />
   )
 }

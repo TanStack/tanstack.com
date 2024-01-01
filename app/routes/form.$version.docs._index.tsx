25149c167d6898d7eee47e723038342bdd137e51
Fülöp Kovács <kovacs.fulop@gmail.com>
2024-01-01 13:34:55 +0100

    Make it possible to select a framework when viewing the framework-independent doc pages of the Form project

--- app/routes/form.$version.docs._index.tsx
+++ app/routes/form.$version.docs._index.tsx
@@ -2,5 +2,5 @@ import type { LoaderFunctionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 
 export const loader = (context: LoaderFunctionArgs) => {
-  throw redirect(context.request.url.replace(/\/docs.*/, '/docs/overview'))
+  throw redirect(context.request.url.replace(/\/docs.*/, '/docs/framework/react/overview'))
 }

087026c0d383e0757a87c84cf325fa6d4142c2b7
Fülöp Kovács <kovacs.fulop@gmail.com>
2024-01-21 00:30:59 +0100

    fix: Regression - the form framework picker doesn't work properly

--- app/projects/store.tsx
+++ app/projects/store.tsx
@@ -132,6 +132,9 @@ export const useReactStoreDocsConfig = () => {
           ...match.params,
           framework: option.value,
         })
+
+        localStorage.setItem('framework', option.value)
+
         navigate(url)
       },
     }

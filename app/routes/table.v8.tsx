f82a2e19cb033a89245d49b9adc02e300fdcab74
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-22 07:22:18 +1100

    Don't load config at /table/v8

--- app/routes/table.v8.tsx
+++ app/routes/table.v8.tsx
@@ -1,17 +1,5 @@
 import { Link, Outlet, useLocation, useSearchParams } from '@remix-run/react'
-import { json } from '@remix-run/node'
 import { DefaultErrorBoundary } from '~/components/DefaultErrorBoundary'
-import { v8branch } from '~/projects/table'
-import { getTanstackDocsConfig } from '~/utils/config'
-
-export const loader = async () => {
-  const repo = 'tanstack/table'
-  const tanstackDocsConfig = await getTanstackDocsConfig(repo, v8branch)
-
-  return json({
-    tanstackDocsConfig,
-  })
-}
 
 export const ErrorBoundary = DefaultErrorBoundary
 

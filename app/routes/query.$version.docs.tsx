150fffda66134c036c8e08b4dd8ff922e14857c1
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-26 08:29:46 +1100

    fix: Invalid versions redirect to latest

--- app/routes/query.$version.docs.tsx
+++ app/routes/query.$version.docs.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
-import { json, useLoaderData } from '@remix-run/react'
+import { json, redirect, useLoaderData } from '@remix-run/react'
 import { seo } from '~/utils/seo'
 import { DocsLayout } from '~/components/DocsLayout'
 import { QueryGGBanner } from '~/components/QueryGGBanner'
@@ -9,12 +9,18 @@ import {
   createLogo,
   repo,
   useQueryDocsConfig,
+  availableVersions,
 } from '~/projects/query'
 import { getTanstackDocsConfig } from '~/utils/config'
 
 export const loader = async (context: LoaderFunctionArgs) => {
   const { version } = context.params
   const branch = getBranch(version)
+
+  if (!availableVersions.concat('latest').includes(version!)) {
+    throw redirect(context.request.url.replace(version!, 'latest'))
+  }
+
   const tanstackDocsConfig = await getTanstackDocsConfig(repo, branch)
 
   return json({

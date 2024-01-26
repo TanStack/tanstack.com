61e969547ae62a75d4cbfdfb7dd4586aee38e452
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-26 18:15:58 +1100

    fix: Redirect old query docs path (#159)

--- app/routes/query.$version.docs.$.tsx
+++ app/routes/query.$version.docs.$.tsx
@@ -2,13 +2,18 @@ import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { repo, getBranch } from '~/projects/query'
 import { DefaultErrorBoundary } from '~/components/DefaultErrorBoundary'
 import { seo } from '~/utils/seo'
-import { useLoaderData, useParams } from '@remix-run/react'
+import { redirect, useLoaderData, useParams } from '@remix-run/react'
 import { loadDocs } from '~/utils/docs'
 import { Doc } from '~/components/Doc'
 
 export const loader = async (context: LoaderFunctionArgs) => {
   const { '*': docsPath, version } = context.params
   const { url } = context.request
+
+  // Temporary fix for old docs structure
+  if (url.includes('/docs/react/')) {
+    throw redirect(url.replace('/docs/react/', '/docs/framework/react/'))
+  }
 
   return loadDocs({
     repo,

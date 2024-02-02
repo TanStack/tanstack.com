2f9d981d810cf78318def2a7c4a0d834d789afbb
fuko <43729152+fulopkovacs@users.noreply.github.com>
2024-02-02 16:49:04 +0100

    Fix old manual redirects (#170)
    * Fix the redirect errors for the Table v7 docs
    
    * Remove the old virtual doc redirects from the router docs

--- app/routes/router.$.tsx
+++ app/routes/router.$.tsx
@@ -1,32 +1,5 @@
 import { redirect } from '@remix-run/node'
-import type { LoaderFunctionArgs } from '@remix-run/node'
 
-export const loader = (context: LoaderFunctionArgs) => {
-  handleRedirects(context)
-
+export const loader = () => {
   return redirect('/router/latest')
-}
-
-function handleRedirects(context: LoaderFunctionArgs) {
-  const url = new URL(context.request.url)
-  // prettier-ignore
-  const reactLocationV2List = [
-    {from: 'docs/overview',to: 'docs/guide/introduction',},
-    {from: 'docs/installation',to: 'docs/guide/installation',},
-    {from: 'docs/api',to: 'docs/api/virtualizer',},
-    {from: 'examples/fixed',to: 'docs/examples/react/fixed',},
-    {from: 'examples/variable',to: 'docs/examples/react/variable',},
-    {from: 'examples/dynamic',to: 'docs/examples/react/dynamic',},
-    {from: 'examples/infinite-scroll',to: 'docs/examples/react/infinite-scroll',},
-    {from: 'examples/padding',to: 'docs/examples/react/padding',},
-    {from: 'examples/smooth-scroll',to: 'docs/examples/react/smooth-scroll',},
-    {from: 'examples/sticky',to: 'docs/examples/react/sticky',},
-    {from: '',to: '',},
-  ]
-
-  reactLocationV2List.forEach((item) => {
-    if (url.pathname.startsWith(`/router/react-location/${item.from}`)) {
-      throw redirect(`/router/v1/${item.to}?from=reactLocationV2`)
-    }
-  })
 }

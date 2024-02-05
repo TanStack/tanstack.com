92236400d4e4bbd167b684e2d1d6fd9c1d471046
fuko <43729152+fulopkovacs@users.noreply.github.com>
2024-02-05 21:14:40 +0100

    Fix the old manual redirects for Virtual and Query (#173)
    * Fix the old manual redirects for virtual
    
    * Fix the old manual redirects for Query v3

--- app/routes/virtual.$.tsx
+++ app/routes/virtual.$.tsx
@@ -1,33 +1,16 @@
-import { redirect } from '@remix-run/node'
 import type { LoaderFunctionArgs } from '@remix-run/node'
+import { redirect } from '@remix-run/node'
+import { reactVirtualV2List } from '~/projects/virtual'
+import { handleRedirects } from '~/utils/handleRedirects.server'
 
 export const loader = (context: LoaderFunctionArgs) => {
-  handleRedirects(context)
+  handleRedirects(
+    reactVirtualV2List,
+    context.request.url,
+    '/virtual/v2',
+    '/virtual/v3',
+    'from=reactVirtualV2'
+  )
 
   return redirect('/virtual/latest')
-}
-
-function handleRedirects(context: LoaderFunctionArgs) {
-  const url = new URL(context.request.url)
-
-  // prettier-ignore
-  const reactVirtualV2List = [
-    {from: 'docs/overview',to: 'docs/guide/introduction',},
-    {from: 'docs/installation',to: 'docs/guide/installation',},
-    {from: 'docs/api',to: 'docs/api/virtualizer',},
-    {from: 'examples/fixed',to: 'docs/examples/react/fixed',},
-    {from: 'examples/variable',to: 'docs/examples/react/variable',},
-    {from: 'examples/dynamic',to: 'docs/examples/react/dynamic',},
-    {from: 'examples/infinite-scroll',to: 'docs/examples/react/infinite-scroll',},
-    {from: 'examples/padding',to: 'docs/examples/react/padding',},
-    {from: 'examples/smooth-scroll',to: 'docs/examples/react/smooth-scroll',},
-    {from: 'examples/sticky',to: 'docs/examples/react/sticky',},
-    {from: '',to: '',},
-  ]
-
-  reactVirtualV2List.forEach((item) => {
-    if (url.pathname.startsWith(`/virtual/v2/${item.from}`)) {
-      throw redirect(`/virtual/v3/${item.to}?from=reactVirtualV2`)
-    }
-  })
 }
--- app/routes/virtual.$version.tsx
+++ app/routes/virtual.$version.tsx
@@ -3,12 +3,25 @@ import { DefaultErrorBoundary } from '~/components/DefaultErrorBoundary'
 import { useClientOnlyRender } from '~/utils/useClientOnlyRender'
 import { RedirectVersionBanner } from '~/components/RedirectVersionBanner'
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import { availableVersions, latestVersion } from '~/projects/virtual'
+import {
+  availableVersions,
+  latestVersion,
+  reactVirtualV2List,
+} from '~/projects/virtual'
+import { handleRedirects } from '~/utils/handleRedirects.server'
 
 export const loader = async (context: LoaderFunctionArgs) => {
   const { version } = context.params
 
   const redirectUrl = context.request.url.replace(version!, 'latest')
+
+  handleRedirects(
+    reactVirtualV2List,
+    context.request.url,
+    '/virtual/v2',
+    '/virtual/v3',
+    'from=reactVirtualV2'
+  )
 
   if (!availableVersions.concat('latest').includes(version!)) {
     throw redirect(redirectUrl)

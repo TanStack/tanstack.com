d7ce031ee2c90167a7dbc1923e550b8b39124931
Corbin Crutchley <git@crutchcorn.dev>
2024-02-07 14:09:55 -0800

    fix: temporarily remove airtable to fix prod (#178)

--- app/server/airtable.ts
+++ app/server/airtable.ts
@@ -1,18 +1,23 @@
 import Airtable from 'airtable'
 
-const airtable = () => new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+// TEMPORARY FIX WHILE AIRTABLE API IS INVALID
+// const airtable = () => new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+const airtable = () => {}
 
 export async function getSponsorsTable() {
-  const base = airtable().base('apppS8mjo4MMR3pif')
-  return base('sponsors')
+  // const base = airtable().base('apppS8mjo4MMR3pif')
+  // return base('sponsors')
+  return {}
 }
 
 export async function getTiersTable() {
-  const base = airtable().base('apppS8mjo4MMR3pif')
-  return base('tiers')
+  // const base = airtable().base('apppS8mjo4MMR3pif')
+  // return base('tiers')
+  return {}
 }
 
 export async function getDiscordInvitesTable() {
-  const base = airtable().base('apppS8mjo4MMR3pif')
-  return base('tiers')
+  // const base = airtable().base('apppS8mjo4MMR3pif')
+  // return base('tiers')
+  return {}
 }
--- app/server/tiers.ts
+++ app/server/tiers.ts
@@ -1,51 +1,52 @@
-import { getTiersTable } from '~/server/airtable'
+// import { getTiersTable } from '~/server/airtable'
 import { graphqlWithAuth } from '~/server/github'
 
 async function getTiersMeta() {
-  try {
-    const tiersTable = await getTiersTable()
-
-    const tiers = await new Promise((resolve, reject) => {
-      let allTiers = []
-      tiersTable.select().eachPage(
-        function page(records, fetchNextPage) {
-          allTiers = [...allTiers, ...records]
-          fetchNextPage()
-        },
-        function done(err) {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(allTiers)
-          }
-        }
-      )
-    })
-
-    return tiers
-  } catch (err: any) {
-    if (err.message === 'An API key is required to connect to Airtable') {
+  // try {
+  //   const tiersTable = await getTiersTable()
+  //
+  //   const tiers = await new Promise((resolve, reject) => {
+  //     let allTiers = []
+  //     tiersTable.select().eachPage(
+  //       function page(records, fetchNextPage) {
+  //         allTiers = [...allTiers, ...records]
+  //         fetchNextPage()
+  //       },
+  //       function done(err) {
+  //         if (err) {
+  //           reject(err)
+  //         } else {
+  //           resolve(allTiers)
+  //         }
+  //       }
+  //     )
+  //   })
+  //
+  //   return tiers
+  // } catch (err: any) {
+    // if (err.message === 'An API key is required to connect to Airtable') {
       console.error('Missing airtable credentials, returning mock data.')
       return []
-    }
-    throw err
-  }
+    // }
+    // throw err
+  // }
 }
 
 async function createTiersMeta(tiersMeta) {
-  const tiersTable = await getTiersTable()
-
-  return new Promise((resolve, reject) => {
-    tiersTable.create(
-      tiersMeta.map((d) => ({ fields: d })),
-      function (err) {
-        if (err) {
-          return reject(err)
-        }
-        resolve()
-      }
-    )
-  })
+  // const tiersTable = await getTiersTable()
+  //
+  // return new Promise((resolve, reject) => {
+  //   tiersTable.create(
+  //     tiersMeta.map((d) => ({ fields: d })),
+  //     function (err) {
+  //       if (err) {
+  //         return reject(err)
+  //       }
+  //       resolve()
+  //     }
+  //   )
+  // })
+  return Promise.resolve()
 }
 
 export async function getTierById(tierId) {

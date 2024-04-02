d47dcdebfbf73c0515d9bc89f8e4bdfc58108523
Tanner Linsley <tannerlinsley@gmail.com>
2024-04-02 17:23:04 -0600

    Update __root.tsx

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -23,9 +23,6 @@ import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary'
 export const Route = createRootRouteWithContext<{
   assets: RouterManagedTag[]
 }>()({
-  headers: () => ({
-    'X-Robots-Tag': 'noindex, nofollow',
-  }),
   meta: () => [
     {
       charSet: 'utf-8',

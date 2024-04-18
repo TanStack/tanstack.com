ddb55f836121b643951a50b97bb2b34ff97f7727
Tanner Linsley <tannerlinsley@gmail.com>
2024-04-18 16:02:57 -0600

    fix: keep prism out of our code blocks!

---
+++ app/hooks/useMounted.ts
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export function useMounted() {
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+  return mounted
+}

c79580e31a79d2a37cca1522929e05e4263c32d5
Tanner Linsley <tannerlinsley@gmail.com>
2024-04-24 15:06:42 -0600

    fix: stop gap fix for hydration mismatches

--- app/client.tsx
+++ app/client.tsx
@@ -1,5 +1,5 @@
 /// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import 'vinxi/client'
 
 import { createRouter } from './router'
@@ -10,4 +10,35 @@ const router = createRouter()
 const app = <StartClient router={router} />
 
 router.hydrate()
-hydrateRoot(document, app)
+
+const handler = (error?: any) => {
+  const errStr = String(error?.message || error)
+  if (
+    errStr.includes('Minified React error #418') ||
+    errStr.includes('Did not expect server HTML to contain')
+  ) {
+    console.error(
+      'The following error occurred while hydrating the app. Falling back to client-side rendering.'
+    )
+    createRoot(document as any).render(app)
+  }
+}
+
+// Patch console.error and console.warn to catch errors
+const originalError = console.error
+const originalWarn = console.warn
+console.error = function (...args: any[]) {
+  handler(args[0])
+  originalError(...args)
+}
+console.warn = function (...args: any[]) {
+  handler(args[0])
+  originalWarn(...args)
+}
+
+try {
+  hydrateRoot(document, app)
+} catch (e) {
+  console.error(e)
+  createRoot(document as any).render(app)
+}

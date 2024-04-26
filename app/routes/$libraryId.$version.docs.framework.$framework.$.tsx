52c039f766e3da7e5e3caf5f692ce6a79bbcea98
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-04-26 17:10:04 +1200

    fix: show the title on the docs page (#212)
    * fix: show the docs title
    
    * chore: cleaner

--- app/routes/$libraryId.$version.docs.framework.$framework.$.tsx
+++ app/routes/$libraryId.$version.docs.framework.$framework.$.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Doc } from '~/components/Doc'
 import { loadDocs } from '~/utils/docs'
 import { getBranch, getLibrary } from '~/libraries'
+import { capitalize } from '~/utils/utils'
 
 export const Route = createFileRoute(
   '/$libraryId/$version/docs/framework/$framework/$'
@@ -21,6 +22,15 @@ export const Route = createFileRoute(
     })
   },
   component: Docs,
+  meta: (ctx) => {
+    const library = getLibrary(ctx.params.libraryId)
+    const tail = `${library.name} ${capitalize(ctx.params.framework)} Docs`
+
+    return seo({
+      title: ctx.loaderData?.title ? `${ctx.loaderData.title} | ${tail}` : tail,
+      description: ctx.loaderData?.description,
+    })
+  },
 })
 
 function Docs() {

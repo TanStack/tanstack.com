c49cbc6d44c450c34c656d72c1953d2a827debef
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-03 10:28:10 -0600

    fix: no more hydration workarounds!

--- app/client.tsx
+++ app/client.tsx
@@ -1,6 +1,6 @@
 /// <reference types="vinxi/types/client" />
 import * as React from 'react'
-import { hydrateRoot, createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 import 'vinxi/client'
 
 import { createRouter } from './router'
@@ -12,33 +12,6 @@ const app = <StartClient router={router} />
 
 router.hydrate()
 
-let root: ReturnType<typeof createRoot>
-
 React.startTransition(() => {
-  root = hydrateRoot(document.getElementById('root')!, app)
-
-  if (root) {
-    //
-  }
+  hydrateRoot(document.getElementById('root')!, app)
 })
-
-// This is a workaround for react hydration completely barfing when
-// browser extensions mutate the DOM before hydration
-// This is a temporary solution until React fixes this issue
-// const checker = setInterval(() => {
-//   if (!document.documentElement) {
-//     console.warn(
-//       'We detected that browser extensions mutated the DOM before hydration and caused some hydration errors. Falling back to client-side rendering.'
-//     )
-//     clearInterval(checker)
-//     clearTimeout(checkedOut)
-//     // const htmlEl = document.createElement('html')
-//     // htmlEl.innerHTML = '<head></head><body></body>'
-//     // document.replaceChildren(htmlEl)
-//     // root.render(app)
-//   }
-// }, 1)
-
-// const checkedOut = setTimeout(() => {
-//   clearInterval(checker)
-// }, 1000)

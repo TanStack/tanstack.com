5fcf46ef823f8b013b664497784dde2021a79fc5
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-03 00:09:16 -0600

    fix: hhhyyyyyyyyyddddddddrrrraaaaattttiiiiioooonnnnnn

--- app/client.tsx
+++ app/client.tsx
@@ -15,26 +15,30 @@ router.hydrate()
 let root: ReturnType<typeof createRoot>
 
 React.startTransition(() => {
-  root = hydrateRoot(document, app)
+  root = hydrateRoot(document.getElementById('root')!, app)
+
+  if (root) {
+    //
+  }
 })
 
 // This is a workaround for react hydration completely barfing when
 // browser extensions mutate the DOM before hydration
 // This is a temporary solution until React fixes this issue
-const checker = setInterval(() => {
-  if (!document.documentElement) {
-    console.warn(
-      'We detected that browser extensions mutated the DOM before hydration and caused some hydration errors. Falling back to client-side rendering.'
-    )
-    clearInterval(checker)
-    clearTimeout(checkedOut)
-    // const htmlEl = document.createElement('html')
-    // htmlEl.innerHTML = '<head></head><body></body>'
-    // document.replaceChildren(htmlEl)
-    root.render(app)
-  }
-}, 1)
+// const checker = setInterval(() => {
+//   if (!document.documentElement) {
+//     console.warn(
+//       'We detected that browser extensions mutated the DOM before hydration and caused some hydration errors. Falling back to client-side rendering.'
+//     )
+//     clearInterval(checker)
+//     clearTimeout(checkedOut)
+//     // const htmlEl = document.createElement('html')
+//     // htmlEl.innerHTML = '<head></head><body></body>'
+//     // document.replaceChildren(htmlEl)
+//     // root.render(app)
+//   }
+// }, 1)
 
-const checkedOut = setTimeout(() => {
-  clearInterval(checker)
-}, 1000)
+// const checkedOut = setTimeout(() => {
+//   clearInterval(checker)
+// }, 1000)

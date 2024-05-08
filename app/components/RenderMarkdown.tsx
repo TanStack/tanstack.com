4981e77b7c8d54b2a37d84df511552f3a158fde4
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-08 15:55:03 -0600

    fix: non-suspending anti-CLS fallback for code blocks

--- app/components/RenderMarkdown.tsx
+++ app/components/RenderMarkdown.tsx
@@ -80,10 +80,24 @@ function CodeBlock(props: React.HTMLProps<HTMLPreElement>) {
         }
       }
 
+  const [copied, setCopied] = React.useState(false)
+  const ref = React.useRef<any>(null)
+
   const code = children?.props.children
 
-  const Children = React.useMemo(() => {
-    return React.lazy(async () => {
+  const [codeElement, setCodeElement] = React.useState(
+    <>
+      <pre ref={ref} className="shiki github-light">
+        <code>{code}</code>
+      </pre>
+      <pre className="shiki tokyo-night bg-gray-900 text-gray-400">
+        <code>{code}</code>
+      </pre>
+    </>
+  )
+
+  React.useLayoutEffect(() => {
+    ;(async () => {
       const themes = ['github-light', 'tokyo-night']
 
       const highlighter = await getHighlighter(lang, themes)
@@ -98,50 +112,37 @@ function CodeBlock(props: React.HTMLProps<HTMLPreElement>) {
         )
       )
 
-      return {
-        default: function Pre() {
-          const [copied, setCopied] = React.useState(false)
-          const ref = React.useRef<HTMLDivElement>(null)
-
-          return (
-            <div
-              className={`${props.className} w-full max-w-full relative not-prose`}
-              style={props.style}
-            >
-              <div className="absolute flex items-stretch bg-white text-sm z-10 border border-gray-500/20 rounded-md -top-3 right-2 dark:bg-gray-800 overflow-hidden divide-x divide-gray-500/20">
-                {lang ? <div className="px-2">{lang}</div> : null}
-                <button
-                  className="px-2 flex items-center text-gray-500 hover:bg-gray-500 hover:text-gray-100 dark:hover:text-gray-200 transition duration-200"
-                  onClick={() => {
-                    navigator.clipboard.writeText(ref.current?.innerText || '')
-                    setCopied(true)
-                    setTimeout(() => setCopied(false), 2000)
-                  }}
-                  aria-label="Copy code to clipboard"
-                >
-                  {copied ? (
-                    <span className="text-xs">Copied!</span>
-                  ) : (
-                    <FaRegCopy />
-                  )}
-                </button>
-              </div>
-              <div
-                // className={`m-0 text-sm rounded-md w-full border border-gray-500/20 dark:border-gray-500/30`}
-                dangerouslySetInnerHTML={{ __html: htmls.join('') }}
-                ref={ref}
-              />
-            </div>
-          )
-        },
-      }
-    })
-  }, [lang, code, props.className, props.style])
+      setCodeElement(
+        <div
+          // className={`m-0 text-sm rounded-md w-full border border-gray-500/20 dark:border-gray-500/30`}
+          dangerouslySetInnerHTML={{ __html: htmls.join('') }}
+          ref={ref}
+        />
+      )
+    })()
+  }, [code, lang])
 
   return (
-    <React.Suspense fallback="">
-      <Children />
-    </React.Suspense>
+    <div
+      className={`${props.className} w-full max-w-full relative not-prose`}
+      style={props.style}
+    >
+      <div className="absolute flex items-stretch bg-white text-sm z-10 border border-gray-500/20 rounded-md -top-3 right-2 dark:bg-gray-800 overflow-hidden divide-x divide-gray-500/20">
+        {lang ? <div className="px-2">{lang}</div> : null}
+        <button
+          className="px-2 flex items-center text-gray-500 hover:bg-gray-500 hover:text-gray-100 dark:hover:text-gray-200 transition duration-200"
+          onClick={() => {
+            navigator.clipboard.writeText(ref.current?.innerText || '')
+            setCopied(true)
+            setTimeout(() => setCopied(false), 2000)
+          }}
+          aria-label="Copy code to clipboard"
+        >
+          {copied ? <span className="text-xs">Copied!</span> : <FaRegCopy />}
+        </button>
+      </div>
+      {codeElement}
+    </div>
   )
 }
 

6406aa56412f9ee3eb38fa2e19e270e8c7096ad8
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-08 13:14:12 -0600

    fix: better latest version redirect for less CLS

--- app/components/RedirectVersionBanner.tsx
+++ app/components/RedirectVersionBanner.tsx
@@ -21,7 +21,7 @@ export function RedirectVersionBanner(props: {
 
   if (![latestVersion, 'latest'].includes(version) && showModal) {
     return (
-      <div className="p-4 bg-blue-500 text-white flex items-center justify-center gap-4">
+      <div className="p-4 bg-white/70 text-black dark:bg-gray-500/40 dark:text-white shadow-xl shadow-black/20 flex items-center justify-center gap-4 fixed top-4 left-1/2 bottom-auto backdrop-blur-sm z-20 -translate-x-1/2 rounded-full overflow-hidden">
         <div>
           You are currently reading <strong>{version}</strong> docs. Redirect to{' '}
           <Link
@@ -39,13 +39,13 @@ export function RedirectVersionBanner(props: {
             version: 'latest',
           }}
           replace
-          className="bg-white text-black py-1 px-2 rounded-md uppercase font-black text-xs"
+          className="bg-black dark:bg-white dark:text-black text-white py-1 px-2 rounded-md uppercase font-black text-xs"
         >
           Latest
         </Link>
         <button
           onClick={() => setShowModal(false)}
-          className="bg-white text-black py-1 px-2 rounded-md uppercase font-black text-xs"
+          className="bg-black dark:bg-white dark:text-black text-white py-1 px-2 rounded-md uppercase font-black text-xs"
         >
           Hide
         </button>

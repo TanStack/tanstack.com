9d3ad2d0a9a52c41d65aaa660b8a923ed179bdc0
Kevin Van Cott <kevinvandy656@gmail.com>
2024-05-19 22:15:42 -0500

    feat: open up relevant file on sandbox load (#232)

--- app/utils/sandbox.ts
+++ app/utils/sandbox.ts
@@ -1,14 +1,28 @@
 import { type Framework } from '~/libraries'
 
-export const getInitialSandboxFileName = (framework: Framework) =>
-  framework === 'svelte'
-    ? 'src/App.svelte'
-    : framework === 'vue'
-    ? 'src/App.vue'
-    : framework === 'solid'
-    ? 'src/App.tsx'
-    : framework === 'angular'
-    ? 'src/app/app.component.ts'
-    : framework === 'lit'
-    ? 'src/main.ts'
-    : 'src/main.tsx'
+export const getInitialSandboxFileName = (
+  framework: Framework,
+  libraryId?: string
+) => {
+  const dir = 'src'
+
+  const file =
+    framework === 'angular'
+      ? 'app.component'
+      : ['svelte', 'vue'].includes(framework)
+      ? 'App'
+      : ['form', 'query'].includes(libraryId!)
+      ? 'index'
+      : 'main'
+
+  const ext =
+    framework === 'svelte'
+      ? 'svelte'
+      : framework === 'vue'
+      ? 'vue'
+      : ['angular', 'lit'].includes(framework)
+      ? 'ts'
+      : 'tsx'
+
+  return `${dir}/${file}.${ext}` as const
+}

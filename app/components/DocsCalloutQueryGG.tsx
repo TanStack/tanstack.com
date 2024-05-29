d7d1150773f2ec2e36a112835c72e08090cf88ee
Lynn Fisher <lynnandtonic@users.noreply.github.com>
2024-05-29 11:00:12 -0700

    Update copy on Query.gg banners (#239)
    * update Query callout on docs pages for launch
    
    * add Query.gg banner on Query landing page
    
    * cleanup
    
    * add countdowns
    
    * style tweaks
    
    * update query.gg sale banner

--- app/components/DocsCalloutQueryGG.tsx
+++ app/components/DocsCalloutQueryGG.tsx
@@ -18,7 +18,10 @@ export function DocsCalloutQueryGG(props: React.HTMLProps<HTMLDivElement>) {
       </div>
       <div className="grid justify-center bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800 z-10">
         <div className="p-2 uppercase text-center place-self-center">
-          <h2 className="mt-1 mb-3 px-2 text-sm font-semibold">Launch sale happening now</h2>
+          <h2 className="mt-1 mb-1 px-2 text-md font-semibold">
+            Launch week sale
+          </h2>
+          <p className="normal-case mb-4 text-sm text-balance">Up to 30% off through June 5th</p>
           <CountdownTimerSmall targetDate="2024-06-05" />
           <a
             className="block m-1 px-4 py-2 bg-gradient-to-r from-rose-500 to-violet-500 text-white rounded uppercase font-bold text-sm text-center"

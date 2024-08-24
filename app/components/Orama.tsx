1687b4b8dbd0924908e0008b5365c6c4f4310ada
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2024-08-24 16:16:43 -0300

    chore: add new orama search widget

--- app/components/Orama.tsx
+++ app/components/Orama.tsx
@@ -1,38 +1,28 @@
-import type {
-  RegisterSearchButtonProps,
-  RegisterSearchBoxProps,
-} from '@orama/searchbox'
-import { OramaClient } from '@oramacloud/client'
-import '@orama/searchbox/dist/index.css'
-
-const oramaInstance = new OramaClient({
-  // The search endpoint for the Orama index
-  endpoint: 'https://cloud.orama.run/v1/indexes/tanstack-dev-ur0ppd',
-  // The public API key for performing search. This is commit-safe.
-  api_key: 'xqfn8QcuImADRGPIlhWTo9cT5UNiqPDj',
-})
-
-export const searchBoxParams: RegisterSearchBoxProps = {
-  oramaInstance,
-  colorScheme: 'system',
-  backdrop: true,
-  facetProperty: 'category',
-  resultsMap: {
-    description: 'content',
+export const searchBoxParams = {
+  index: {
+    api_key: 'xqfn8QcuImADRGPIlhWTo9cT5UNiqPDj',
+    endpoint: 'https://cloud.orama.run/v1/indexes/tanstack-dev-ur0ppd',
   },
+  colorScheme: 'system' as 'light' | 'dark' | 'system',
+  facetProperty: 'category',
+  resultMap: {
+    description: 'content',
+    section: 'category',
+  },
+
   themeConfig: {
-    light: {},
-    dark: {
-      '--backdrop-bg-color': '#0d103591',
+    colors: {
+      light: {},
+      dark: {
+        '--backdrop-background-color-primary': 'rgba(0, 0, 0, 0.7)',
+      },
     },
   },
-  searchMode: 'hybrid',
   searchParams: {
     threshold: 0,
   },
 }
 
-export const searchButtonParams: RegisterSearchButtonProps = {
+export const searchButtonParams = {
   colorScheme: 'system',
-  fullWidth: true,
 }

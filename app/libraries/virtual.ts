47858e3e665ab13beaec03819227cfab5e854bc6
Garrett Darnell <12160067+garrettld@users.noreply.github.com>
2024-08-28 20:29:21 -0400

    feat(virtual): add Angular as a supported framework (#265)

--- app/libraries/virtual.ts
+++ app/libraries/virtual.ts
@@ -6,7 +6,7 @@ export const virtualProject: Library = {
   cardStyles: `shadow-xl shadow-purple-700/20 dark:shadow-lg dark:shadow-purple-500/30 text-purple-500 border-2 border-transparent hover:border-current`,
   to: '/virtual',
   tagline: `Headless UI for Virtualizing Large Element Lists`,
-  description: `Virtualize only the visible content for massive scrollable DOM nodes at 60FPS in TS/JS, React, Vue, Solid, Svelte & Lit while retaining 100% control over markup and styles.`,
+  description: `Virtualize only the visible content for massive scrollable DOM nodes at 60FPS in TS/JS, React, Vue, Solid, Svelte, Lit & Angular while retaining 100% control over markup and styles.`,
   ogImage: 'https://github.com/tanstack/query/raw/main/media/header.png',
   bgStyle: 'bg-purple-500',
   textStyle: 'text-purple-500',
@@ -17,7 +17,7 @@ export const virtualProject: Library = {
   colorFrom: 'from-purple-500',
   colorTo: 'to-violet-600',
   textColor: 'text-purple-600',
-  frameworks: ['react', 'solid', 'vue', 'svelte', 'lit'],
+  frameworks: ['react', 'solid', 'vue', 'svelte', 'lit', 'angular'],
   defaultDocs: 'framework/react/overview',
   scarfId: '32372eb1-91e0-48e7-8df1-4808a7be6b94',
 }

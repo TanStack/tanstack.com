c1ab154bf2b4c30c3cbe80424b3df392c2d04579
Tanner Linsley <tannerlinsley@gmail.com>
2024-11-15 21:43:35 -0700

    fix: vercel output

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+  },
   vite: {
     plugins: [tsConfigPaths()],
   },

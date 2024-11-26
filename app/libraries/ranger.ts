f96dc67f966c5ebff50b6ca0b376dc17cb43350e
Shawn Erquhart <shawn@erquh.art>
2024-11-26 16:42:06 -0500

    improve library config typing fix

--- app/libraries/ranger.ts
+++ app/libraries/ranger.ts
@@ -1,6 +1,6 @@
 import { Library } from '.'
 
-export const rangerProject: Library = {
+export const rangerProject = {
   id: 'ranger',
   name: 'TanStack Ranger',
   cardStyles: `shadow-xl shadow-pink-700/20 dark:shadow-lg dark:shadow-pink-500/30 text-pink-500 border-2 border-transparent hover:border-current`,
@@ -8,7 +8,7 @@ export const rangerProject: Library = {
   tagline: `Headless range and multi-range slider utilities.`,
   description: `Headless, lightweight, and extensible primitives for building range and multi-range sliders.`,
   ogImage: 'https://github.com/tanstack/ranger/raw/main/media/headerv1.png',
-  // badge: 'new',
+  badge: undefined,
   bgStyle: 'bg-pink-500',
   textStyle: 'text-pink-500',
   repo: 'tanstack/ranger',
@@ -20,4 +20,4 @@ export const rangerProject: Library = {
   textColor: 'text-pink-500',
   frameworks: ['react'],
   scarfId: 'dd278e06-bb3f-420c-85c6-6e42d14d8f61',
-}
+} satisfies Library

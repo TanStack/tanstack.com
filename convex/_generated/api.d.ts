90c763642ff2272154f5b37dad38f8d8bfadefce
Shawn Erquhart <shawn@erquh.art>
2024-12-11 15:05:24 -0500

    Use fresh data w/ Convex for open source stats (#309)
    * fix svg import ts errors
    
    * fix type errors affecting libraries/index
    
    * add convex client
    
    * add more data
    
    * add dependents count
    
    * animate numbers
    
    * use averages to count up npm downloads
    
    * smooth animations with regular font
    
    * switch to published oss-stats component
    
    * update scripts
    
    * use default convex instance for local dev
    
    * improve library config typing fix
    
    * add note for hardcoded convex url
    
    * add github dependent live counter
    
    * remove unused counter component
    
    * use incrementing github dependent counter
    
    * fix convex logo for light mode
    
    * update convex link

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,122 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as stats from "../stats.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  stats: typeof stats;
+}>;
+declare const fullApiWithMounts: typeof fullApi;
+
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {
+  ossStats: {
+    lib: {
+      getGithubOwner: FunctionReference<
+        "query",
+        "internal",
+        { owner: string },
+        any
+      >;
+      getNpmOrg: FunctionReference<"query", "internal", { name: string }, any>;
+      sync: FunctionReference<
+        "action",
+        "internal",
+        {
+          githubAccessToken: string;
+          githubOwners: Array<string>;
+          minStars: number;
+          npmOrgs: Array<string>;
+        },
+        any
+      >;
+      updateGithubOwner: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          contributorCount?: number;
+          dependentCount?: number;
+          owner: string;
+          starCount?: number;
+        },
+        any
+      >;
+      updateGithubRepoStars: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          githubAccessToken: string;
+          name: string;
+          owner: string;
+          starCount?: number;
+        },
+        any
+      >;
+      updateGithubRepos: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          repos: Array<{
+            contributorCount: number;
+            dependentCount: number;
+            dependentCountPrevious?: { count: number; updatedAt: number };
+            name: string;
+            owner: string;
+            starCount: number;
+          }>;
+        },
+        any
+      >;
+      updateNpmOrg: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          dayOfWeekAverages: Array<number>;
+          downloadCount: number;
+          name: string;
+        },
+        any
+      >;
+      updateNpmPackages: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          packages: Array<{
+            dayOfWeekAverages: Array<number>;
+            downloadCount: number;
+            name: string;
+          }>;
+        },
+        any
+      >;
+    };
+  };
+};

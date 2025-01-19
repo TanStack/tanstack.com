79122c6916699cd5ba659ff6a920f8f18f705aa0
Tanner Linsley <tannerlinsley@gmail.com>
2025-01-18 22:47:33 -0700

    fix: dark styles

--- app/components/LibraryFeatureHighlights.tsx
+++ app/components/LibraryFeatureHighlights.tsx
@@ -8,13 +8,13 @@ export function LibraryFeatureHighlights({
   return featureHighlights?.length ? (
     <div
       className={`text-lg grid grid-cols-1 md:grid-cols-2 ${
-        featureHighlights.length > 3 ? 'lg:grid-cols-4' : 'lg:grid-cols-3'
-      } gap-8 p-8 max-w-[1200px] mx-auto`}
+        featureHighlights.length > 3 ? 'xl:grid-cols-4' : 'xl:grid-cols-3'
+      } gap-x-8 gap-y-12 p-8 max-w-[1200px] mx-auto`}
     >
       {featureHighlights?.map((featureHighlight) => {
         return (
-          <div className="p-4 pt-6 flex-1 flex flex-col gap-4 items-center bg-white/50 shadow-xl rounded-xl relative">
-            <div className="text-3xl text-center p-2 rounded-full shadow-md absolute top-0 left-1/2 -translate-x-1/2 -translate-y-2/3 bg-white">
+          <div className="p-4 pt-6 flex-1 flex flex-col gap-4 items-center bg-white/50 dark:bg-black/40 shadow-xl rounded-xl relative">
+            <div className="text-3xl text-center p-2 rounded-full shadow-md absolute top-0 left-1/2 -translate-x-1/2 -translate-y-2/3 bg-white dark:bg-black/50">
               {featureHighlight.icon}
             </div>
             <div className="flex flex-col gap-4">

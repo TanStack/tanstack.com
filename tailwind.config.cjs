ff12283199f8f822f960d8d3568832cc8d35e46a
Tanner Linsley <tannerlinsley@gmail.com>
2025-01-20 22:47:17 -0700

    feat: font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        sans: `"Inter", ui-sans-serif,system-ui,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji"`,
+      },
       zIndex: {
         '-10': '-10',
       },

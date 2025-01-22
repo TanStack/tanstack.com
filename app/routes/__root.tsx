dd8e2f32124ce50bcb296a97a9b17eabe85cd66f
Tanner Linsley <tannerlinsley@gmail.com>
2025-01-22 09:07:08 -0700

    fix: update TanStack Start

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -73,7 +73,7 @@ export const Route = createRootRouteWithContext<{
       { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
       { rel: 'icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: '' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',

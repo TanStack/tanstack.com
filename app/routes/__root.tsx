752c28898c0d2103814ab1566e53680443cd837a
Tanner Linsley <tannerlinsley@gmail.com>
2025-03-16 22:47:14 -0600

    gtm

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -89,7 +89,7 @@ export const Route = createRootRouteWithContext<{
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TPHCWWDB');
+          })(window,document,'script','dataLayer','GTM-5N57KQT4');
         `,
       },
       // Google Analytics script (gtag.js)
@@ -228,6 +228,15 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           </div>
         ) : null}
         <CookieConsent />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5N57KQT4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="gtm"
+          ></iframe>
+        </noscript>
         <Scripts />
       </body>
     </html>

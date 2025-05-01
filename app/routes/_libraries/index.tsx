6cf25d8f28cb707948b0979cb495be828efd1aa9
Tanner Linsley <tannerlinsley@gmail.com>
2025-05-01 12:19:57 -0600

    home page ad

--- app/routes/_libraries/index.tsx
+++ app/routes/_libraries/index.tsx
@@ -19,6 +19,7 @@ import { partners } from '../../utils/partners'
 import OpenSourceStats from '~/components/OpenSourceStats'
 import splashLightImg from '~/images/splash-light.png'
 import splashDarkImg from '~/images/splash-dark.png'
+import { GadFooter } from '~/components/GoogleScripts'
 
 export const textColors = [
   `text-rose-500`,
@@ -388,16 +389,33 @@ function Index() {
         <hr className="my-16 border-gray-200 dark:border-gray-500/10" />
         <div className={`lg:max-[400px] px-4 mx-auto`}>
           <div className="flex flex-col gap-4 items-center">
-            <div className="shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-800 dark:text-white max-w-[250px] mx-auto">
-              <Carbon />
+            <div className="shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-800 dark:text-white mx-auto">
+              <GadFooter />
             </div>
-            <span
-              className="text-[.7rem] bg-gray-500 bg-opacity-10 py-1 px-2 rounded text-gray-500
-                dark:bg-opacity-20 self-center text-center"
+            <div
+              className="text-xs bg-gray-500 bg-opacity-10 py-2 px-4 rounded text-gray-500
+                dark:bg-opacity-20 self-center text-center w-[500px] max-w-full space-y-2"
             >
-              This ad helps us be happy about our invested time and not burn out
-              and rage-quit OSS. Yay money! 😉
-            </span>
+              <div>
+                <span className="font-medium italic">
+                  An ad on an open source project?
+                </span>{' '}
+                <span className="font-black">What is this, 1999?</span>
+              </div>
+              <div>
+                <span className="font-medium italic">Please...</span> TanStack
+                is 100% privately owned, with no paid products, venture capital,
+                or acquisition plans. We're a small team dedicated to creating
+                software used by millions daily. What did you expect?
+              </div>
+              <div>
+                <Link to="/ethos" className="text-gray-500 font-bold underline">
+                  Check out our ethos
+                </Link>{' '}
+                to learn more about how we plan on sticking around (and staying
+                relevant) for the long-haul.
+              </div>
+            </div>
           </div>
         </div>
         <hr className="my-16 border-gray-200 dark:border-gray-500/10" />

b2f1b7c3504a2219d42be122b7498eb819b05ab7
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-02 17:38:27 +1200

    refactor: use `Intl.ListFormat` for joining the authors

--- app/utils/blog.ts
+++ app/utils/blog.ts
@@ -1,15 +1,12 @@
+const joiner = new Intl.ListFormat('en-US', {
+  style: 'long',
+  type: 'conjunction',
+})
+
 export function formatAuthors(authors: Array<string>) {
   if (!authors.length) {
     return 'TanStack'
   }
 
-  if (authors.length === 1) {
-    return authors[0]
-  }
-
-  if (authors.length === 2) {
-    return authors.join(' and ')
-  }
-
-  return authors.slice(0, -1).join(', ') + ', and ' + authors.slice(-1)
+  return joiner.format(authors)
 }

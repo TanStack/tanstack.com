6244192caafdc87ece44a98e410f0f7faf60b960
Lynn Fisher <lynnandtonic@users.noreply.github.com>
2025-05-05 08:59:21 -0700

    Add query.gg launch sale banner (#395)
    * add launch banners to query page and docs
    
    * style: prettier formatting
    
    ---------
    
    Co-authored-by: SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>

--- app/routes/_libraries/query.$version.index.tsx
+++ app/routes/_libraries/query.$version.index.tsx
@@ -7,7 +7,8 @@ import { Carbon } from '~/components/Carbon'
 import { Footer } from '~/components/Footer'
 import { TbHeartHandshake } from 'react-icons/tb'
 import SponsorPack from '~/components/SponsorPack'
-import { QueryGGBanner } from '~/components/QueryGGBanner'
+// import { QueryGGBanner } from '~/components/QueryGGBanner'
+import { QueryGGBannerSale } from '~/components/QueryGGBannerSale'
 import { queryProject } from '~/libraries/query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Framework, getBranch, getLibrary } from '~/libraries'
@@ -48,6 +49,7 @@ export default function VersionIndex() {
       <div className="flex flex-1 min-h-0 relative justify-center overflow-x-hidden">
         <div className="flex flex-col gap-20 md:gap-32 max-w-full py-32">
           <div className="flex flex-col items-center gap-8 text-center px-4">
+            <QueryGGBannerSale />
             <h1 className="font-black flex gap-3 items-center text-4xl md:text-6xl lg:text-7xl xl:text-8xl uppercase [letter-spacing:-.05em]">
               <span>TanStack</span>
               <span className={twMerge(gradientText)}>Query</span>
@@ -83,9 +85,18 @@ export default function VersionIndex() {
               >
                 Read the Docs
               </Link>
-              <p>(or check out our official course 👇)</p>
+              <p>
+                (or{' '}
+                <a
+                  href="https://query.gg?s=tanstack"
+                  className="font-semibold underline"
+                >
+                  check out our official course
+                </a>
+                . It’s on sale!)
+              </p>
             </div>
-            <QueryGGBanner />
+            {/* <QueryGGBanner /> */}
           </div>
           <LibraryFeatureHighlights
             featureHighlights={library.featureHighlights}

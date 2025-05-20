0014da843d5d3c9f53f0b07b9219a9710f89a594
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-20 16:46:33 +1200

    implementation for `.md` suffixed routes
    Co-Authored-By: fuko <43729152+fulopkovacs@users.noreply.github.com>

--- src/routes/$libraryId/$version.docs.{$}[.]md.tsx
+++ src/routes/$libraryId/$version.docs.{$}[.]md.tsx
@@ -1,5 +1,30 @@
+import { getBranch, getLibrary } from '~/libraries'
+import { loadDocs } from '~/utils/docs'
+
 export const ServerRoute = createServerFileRoute().methods({
-  GET: async ({ request }) => {
-    return new Response('Foo bar')
+  GET: async ({ request, params }) => {
+    const url = new URL(request.url)
+
+    const { libraryId, version, _splat: docsPath } = params
+    const library = getLibrary(libraryId)
+    const root = library.docsRoot || 'docs'
+
+    const doc = await loadDocs({
+      repo: library.repo,
+      branch: getBranch(library, version),
+      docsPath: `${root}/${docsPath}`,
+      currentPath: url.pathname,
+      redirectPath: `/${library.id}/${version}/docs/overview`,
+    })
+
+    const markdownContent = `# ${doc.title}\n${doc.content}`
+    const filename = (docsPath || 'file').split('/').join('-')
+
+    return new Response(markdownContent, {
+      headers: {
+        'Content-Type': 'text/markdown',
+        'Content-Disposition': `inline; filename="${filename}.md"`,
+      },
+    })
   },
 })

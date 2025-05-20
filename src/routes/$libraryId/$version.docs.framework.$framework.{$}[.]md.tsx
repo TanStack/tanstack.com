5aaaea4611b47caf1c6877e89a7d84734edc0501
fuko <43729152+fulopkovacs@users.noreply.github.com>
2025-05-21 00:38:16 +0200

    feat: directly serve library document markdown
    Co-authored-by: SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>

---
+++ src/routes/$libraryId/$version.docs.framework.$framework.{$}[.]md.tsx
@@ -0,0 +1,32 @@
+import { getBranch, getLibrary } from '~/libraries'
+import { loadDocs } from '~/utils/docs'
+
+export const ServerRoute = createServerFileRoute().methods({
+  GET: async ({ request, params }) => {
+    const url = new URL(request.url)
+
+    const { libraryId, version, framework, _splat: docsPath } = params
+    const library = getLibrary(libraryId)
+    const root = library.docsRoot || 'docs'
+
+    const doc = await loadDocs({
+      repo: library.repo,
+      branch: getBranch(library, version),
+      docsPath: `${root}/framework/${framework}/${docsPath}`,
+      currentPath: url.pathname,
+      redirectPath: `/${library.id}/${version}/docs/overview`,
+    })
+
+    const markdownContent = `# ${doc.title}\n${doc.content}`
+    const filename = (docsPath || 'file').split('/').join('-')
+
+    return new Response(markdownContent, {
+      headers: {
+        'Content-Type': 'text/markdown',
+        'Content-Disposition': `inline; filename="${filename}.md"`,
+        'Cache-Control': 'public, max-age=0, must-revalidate',
+        'Cdn-Cache-Control': 'max-age=300, stale-while-revalidate=300, durable',
+      },
+    })
+  },
+})

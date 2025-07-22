e89e922636a0073dc9993179856e915898f167dd
Tijn Roozen <37410925+TijnR@users.noreply.github.com>
2025-07-23 01:12:01 +0200

    fix: icons of the maintainer card overflowing (#445)

--- src/routes/_libraries/index.tsx
+++ src/routes/_libraries/index.tsx
@@ -388,7 +388,7 @@ function Index() {
 
         <div className="px-4 lg:max-w-screen-lg md:mx-auto">
           <h3 className={`text-4xl font-light mb-6`}>Core Maintainers</h3>
-          <div className={`grid gap-6 grid-cols-2 lg:grid-cols-3`}>
+          <div className="grid gap-6 grid-cols-2 lg:grid-cols-3">
             {coreMaintainers.map((maintainer) => (
               <MaintainerCard key={maintainer.github} maintainer={maintainer} />
             ))}

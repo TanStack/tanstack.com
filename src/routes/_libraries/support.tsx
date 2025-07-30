180b6bf3affeaa191e278e0425aa18a570610223
Tanner Linsley <tannerlinsley@gmail.com>
2025-07-30 17:09:30 -0600

    styles

--- src/routes/_libraries/support.tsx
+++ src/routes/_libraries/support.tsx
@@ -77,7 +77,7 @@ function SupportComp() {
             to="/paid-support"
             className="group bg-white/80 shadow-xl shadow-gray-500/20 rounded-lg dark:border border-gray-500/20 dark:bg-black/40 dark:shadow-none overflow-hidden hover:shadow-2xl transition-shadow duration-200 flex flex-column "
           >
-            <div className="bg-gradient-to-br from-blue-600 to-purple-600 p-6 text-white">
+            <div className="bg-gradient-to-br from-green-600 to-cyan-600 p-6 text-white">
               <h3 className="text-xl font-semibold mb-2">Paid Support</h3>
               <p className="text-sm text-white/80 mb-4">
                 Expert consultation and enterprise support

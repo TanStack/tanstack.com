e6ffe3caef3bdea81a1e0334281c8a7f328da529
Leonardo Montini <lion.48m@gmail.com>
2025-08-07 10:16:41 +0200

    fix: janky knob animation

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import { useTheme } from './ThemeProvider'
-import { twMerge } from 'tailwind-merge'
 
 export function ThemeToggle() {
   const { toggleMode } = useTheme()
@@ -17,9 +16,7 @@ export function ThemeToggle() {
   return (
     <div
       onClick={handleToggleMode}
-      className={twMerge(
-        `w-12 h-6 bg-gray-500/10 dark:bg-gray-800 rounded-full flex items-center justify-between cursor-pointer relative transition-all`
-      )}
+      className={`w-12 h-6 bg-gray-500/10 dark:bg-gray-800 rounded-full flex items-center justify-between cursor-pointer relative transition-all`}
     >
       <div className="flex-1 flex items-center justify-between px-1.5">
         <FaSun
@@ -37,7 +34,7 @@ export function ThemeToggle() {
       <div
         className="absolute w-6 h-6 rounded-full shadow-md shadow-black/20 bg-white dark:bg-gray-400 transition-all duration-300 ease-in-out
                    auto:left-1/2 auto:-translate-x-1/2 auto:scale-0 auto:opacity-0
-                   left-full -translate-x-full scale-75
+                   left-0 translate-x-full scale-75
                    dark:left-0 dark:translate-x-0 dark:scale-75"
       />
     </div>

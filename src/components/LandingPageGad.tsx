5febfde457c63c768a2af1b24a20fc262d7f4ffd
Kevin Van Cott <kevinvandy656@gmail.com>
2025-08-11 19:09:46 -0500

    fix bad landing page gad resolution

--- src/components/LandingPageGad.tsx
+++ src/components/LandingPageGad.tsx
@@ -27,28 +27,14 @@ export default function LandingPageGad() {
               software used by millions daily. What did you expect?
             </div>
             <div>
-              <div>
-                <span className="font-medium italic">
-                  An ad on an open source project?
-                </span>{' '}
-                <span className="font-black">What is this, 1999?</span>
-              </div>
-              <div>
-                <span className="font-medium italic">Please...</span> TanStack
-                is 100% privately owned, with no paid products, venture capital,
-                or acquisition plans. We're a small team dedicated to creating
-                software used by millions daily. What did you expect?
-              </div>
-              <div>
-                <Link
-                  to="/ethos"
-                  className="text-gray-600 dark:text-gray-200 font-bold underline"
-                >
-                  Check out our ethos
-                </Link>{' '}
-                to learn more about how we plan on sticking around (and staying
-                relevant) for the long-haul.
-              </div>
+              <Link
+                to="/ethos"
+                className="text-gray-600 dark:text-gray-200 font-bold underline"
+              >
+                Check out our ethos
+              </Link>{' '}
+              to learn more about how we plan on sticking around (and staying
+              relevant) for the long-haul.
             </div>
           </div>
         </div>

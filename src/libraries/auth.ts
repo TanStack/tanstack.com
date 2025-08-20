6afd5937224e7ea0d31b6a88e00331ed5ef688e6
Austin Montoya <austin@liminal.sh>
2025-08-19 20:26:44 -0700

    add multiple fallbacks

--- src/libraries/auth.ts
+++ src/libraries/auth.ts
@@ -3,10 +3,9 @@ import { convex } from '@convex-dev/better-auth/plugins'
 import { betterAuth } from 'better-auth'
 import { betterAuthComponent } from '../../convex/auth'
 import { type GenericCtx } from '../../convex/_generated/server'
-import { env } from '~/utils/env'
 
 // You'll want to replace this with an environment variable
-const siteUrl = process.env.BETTER_AUTH_BASE_URL || "http://localhost:3000";
+const siteUrl = process.env.BETTER_AUTH_BASE_URL || process.env.URL || "http://localhost:3000";
 
 export const createAuth = (ctx: GenericCtx) =>
   // Configure your Better Auth instance here

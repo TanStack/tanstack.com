a9b4aa3fa50193c37ae637db3ae0bb4c8b9f3abd
Tanner Linsley <tannerlinsley@gmail.com>
2025-09-03 00:36:25 -0600

    no index on users

--- convex/schema.ts
+++ convex/schema.ts
@@ -32,7 +32,8 @@ const schema = defineSchema({
       v.union(...validCapabilities.map((cap) => v.literal(cap)))
     ),
     adsDisabled: v.optional(v.boolean()),
-  }).index('by_email', ['email']),
+  }),
+  // .index('by_email', ['email']),
 })
 
 export default schema

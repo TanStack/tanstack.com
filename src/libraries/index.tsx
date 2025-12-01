04e205c6821618b102c3cc6b473a4661691aa737
Kevin Van Cott <kevinvandy656@gmail.com>
2025-12-01 12:55:47 -0600

    add preact store, standardize framework font-colors (#553)
    * add preact store, standardize framework font-colors
    
    * prettier
    # Conflicts:
    #	src/libraries/store.tsx

--- src/libraries/index.tsx
+++ src/libraries/index.tsx
@@ -21,25 +21,69 @@ import { dbProject } from './db'
 import { devtoolsProject } from './devtools'
 
 export const frameworkOptions = [
-  { label: 'React', value: 'react', logo: reactLogo, color: 'bg-blue-500' },
-  { label: 'Preact', value: 'preact', logo: preactLogo, color: 'bg-blue-400' },
-  { label: 'Vue', value: 'vue', logo: vueLogo, color: 'bg-green-500' },
+  {
+    label: 'React',
+    value: 'react',
+    logo: reactLogo,
+    color: 'bg-blue-500',
+    fontColor: 'text-sky-500',
+  },
+  {
+    label: 'Preact',
+    value: 'preact',
+    logo: preactLogo,
+    color: 'bg-purple-500',
+    fontColor: 'text-purple-500',
+  },
+  {
+    label: 'Vue',
+    value: 'vue',
+    logo: vueLogo,
+    color: 'bg-green-500',
+    fontColor: 'text-green-500',
+  },
   {
     label: 'Angular',
     value: 'angular',
     logo: angularLogo,
     color: 'bg-red-500',
+    fontColor: 'text-fuchsia-500',
   },
-  { label: 'Solid', value: 'solid', logo: solidLogo, color: 'bg-blue-600' },
-  { label: 'Lit', value: 'lit', logo: litLogo, color: 'bg-orange-500' },
+  {
+    label: 'Solid',
+    value: 'solid',
+    logo: solidLogo,
+    color: 'bg-blue-600',
+    fontColor: 'text-blue-600',
+  },
+  {
+    label: 'Lit',
+    value: 'lit',
+    logo: litLogo,
+    color: 'bg-emerald-500',
+    fontColor: 'text-emerald-500',
+  },
   {
     label: 'Svelte',
     value: 'svelte',
     logo: svelteLogo,
     color: 'bg-orange-600',
+    fontColor: 'text-orange-600',
   },
-  { label: 'Qwik', value: 'qwik', logo: qwikLogo, color: 'bg-purple-500' },
-  { label: 'Vanilla', value: 'vanilla', logo: jsLogo, color: 'bg-yellow-500' },
+  {
+    label: 'Qwik',
+    value: 'qwik',
+    logo: qwikLogo,
+    color: 'bg-indigo-500',
+    fontColor: 'text-indigo-500',
+  },
+  {
+    label: 'Vanilla',
+    value: 'vanilla',
+    logo: jsLogo,
+    color: 'bg-yellow-500',
+    fontColor: 'text-yellow-500',
+  },
 ] as const
 
 export type Framework = (typeof frameworkOptions)[number]['value']

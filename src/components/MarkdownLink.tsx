ad85fd6e3076ff0cb81f890af26efb83a857b4c2
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-12-05 22:38:32 +1100

    feat: Improve handling of relative links in markdown (#563)
    * WIP: MarkdownLink
    
    * Tweak MarkdownLink
    
    * Handle relative link without ./

--- src/components/MarkdownLink.tsx
+++ src/components/MarkdownLink.tsx
@@ -1,27 +1,42 @@
 import { Link } from '@tanstack/react-router'
 import type { HTMLProps } from 'react'
 
+function isRelativeLink(link: string) {
+  return (
+    !link.startsWith(`/`) &&
+    !link.startsWith('http://') &&
+    !link.startsWith('https://') &&
+    !link.startsWith('//') &&
+    !link.startsWith('#') &&
+    !link.startsWith('mailto:')
+  )
+}
+
 export function MarkdownLink({
   href: hrefProp,
   ...rest
 }: HTMLProps<HTMLAnchorElement>) {
-  if (
-    hrefProp?.startsWith('http') ||
-    hrefProp?.startsWith('#') ||
-    hrefProp?.startsWith('//')
-  ) {
-    // eslint-disable-next-line jsx-a11y/anchor-has-content
+  if (!isRelativeLink(hrefProp ?? '')) {
     return <a {...rest} href={hrefProp} />
   }
 
   const [hrefWithoutHash, hash] = hrefProp?.split('#') ?? []
-  const [to] = hrefWithoutHash?.split('.md') ?? []
+  let hrefWithoutMd = hrefWithoutHash.replace('.md', '')
+
+  // Force relative links to resolve one level higher
+  if (hrefWithoutMd.startsWith('../')) {
+    hrefWithoutMd = hrefWithoutMd.replace(/^\.\.\//gm, '../../')
+  } else if (hrefWithoutMd.startsWith('./')) {
+    hrefWithoutMd = hrefWithoutMd.replace(/^\.\//gm, '../')
+  } else {
+    hrefWithoutMd = `../${hrefWithoutMd}`
+  }
 
   return (
     <Link
       {...rest}
       unsafeRelative="path"
-      to={to}
+      to={hrefWithoutMd}
       hash={hash}
       preload={undefined}
       ref={undefined}

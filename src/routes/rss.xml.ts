6ae3bd48400a9886769310c6286c5b8f2f7ce311
Sarah Gerrard <hello@sarahgerrard.me>
2026-01-06 23:40:54 +0000

    wip

---
+++ src/routes/rss.xml.ts
@@ -0,0 +1,71 @@
+import { createAPIFileRoute } from '@tanstack/react-start/api'
+import { getPublishedPosts, formatAuthors } from '~/utils/blog'
+
+function escapeXml(unsafe: string): string {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+function generateRSSFeed() {
+  const posts = getPublishedPosts().slice(0, 50) // Most recent 50 posts
+  const siteUrl = 'https://tanstack.com'
+  const buildDate = new Date().toUTCString()
+
+  const rssItems = posts
+    .map((post) => {
+      const postUrl = `${siteUrl}/blog/${post.slug}`
+      const pubDate = new Date(post.published).toUTCString()
+      const author = formatAuthors(post.authors)
+      
+      // Use excerpt if available, otherwise try to get first paragraph from content
+      let description = post.excerpt || ''
+      if (!description && post.content) {
+        // Extract first paragraph after frontmatter
+        const contentWithoutFrontmatter = post.content.replace(/^---[\s\S]*?---/, '').trim()
+        const firstParagraph = contentWithoutFrontmatter.split('\n\n')[0]
+        description = firstParagraph.replace(/!\[[^\]]*\]\([^)]*\)/g, '') // Remove images
+      }
+
+      return `
+    <item>
+      <title>${escapeXml(post.title)}</title>
+      <link>${escapeXml(postUrl)}</link>
+      <guid isPermaLink="true">${escapeXml(postUrl)}</guid>
+      <pubDate>${pubDate}</pubDate>
+      <author>${escapeXml(author)}</author>
+      <description>${escapeXml(description)}</description>
+      ${post.headerImage ? `<enclosure url="${escapeXml(siteUrl + post.headerImage)}" type="image/png" />` : ''}
+    </item>`
+    })
+    .join('')
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>TanStack Blog</title>
+    <link>${siteUrl}/blog</link>
+    <description>The latest news and updates from TanStack</description>
+    <language>en-us</language>
+    <lastBuildDate>${buildDate}</lastBuildDate>
+    <atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml" />
+    ${rssItems}
+  </channel>
+</rss>`
+}
+
+export const APIRoute = createAPIFileRoute('/rss.xml')({
+  GET: async () => {
+    const rss = generateRSSFeed()
+
+    return new Response(rss, {
+      headers: {
+        'Content-Type': 'application/xml; charset=utf-8',
+        'Cache-Control': 'public, max-age=300, s-maxage=3600',
+      },
+    })
+  },
+})

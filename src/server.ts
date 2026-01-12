1c1f52bdd7a5e70b86d205e8922bd7c717a0ed40
Dominik Dorfmeister <office@dorfmeister.cc>
2026-01-12 15:32:42 +0100

    feat: sentry integration (#641)
    * feat: sentry integration
    
    * fix: netlify build command
    
    * set --import node_options
    
    * revert
    
    * test error
    
    * Revert "test error"
    
    This reverts commit f25daa344b0ffe4799b36ab968938fe306fab999.
    
    * feat: import instrument.server.mjs in server entry point

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import './instrument.server.mjs'
+
+import handler, { createServerEntry } from '@tanstack/react-start/server-entry'
+
+export default createServerEntry({
+  fetch(request) {
+    return handler.fetch(request)
+  },
+})

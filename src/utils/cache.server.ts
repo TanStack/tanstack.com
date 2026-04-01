b79952e4fb5c3e9cfb1d17fba1b2f0d63a2bf862
Sarah Gerrard <gerrardsarah@gmail.com>
2026-04-01 13:40:33 -0700

    feat: enhance caching mechanism with pending document cache support

--- src/utils/cache.server.ts
+++ src/utils/cache.server.ts
@@ -1,8 +1,8 @@
-import { LRUCache } from "lru-cache";
+import { LRUCache } from 'lru-cache'
 
 declare global {
   // @ts-expect-error - globalThis.docCache is set in the server entrypoint
-  var docCache: LRUCache<string, unknown>;
+  var docCache: LRUCache<string, unknown>
 }
 
 const docCache =
@@ -11,24 +11,24 @@ const docCache =
   (globalThis.docCache = new LRUCache<string, unknown>({
     max: 300,
     // ttl: 1,
-    ttl: process.env.NODE_ENV === "production" ? 1 : 1000000,
-  }));
+    ttl: process.env.NODE_ENV === 'production' ? 1 : 1000000,
+  }))
 
-const pendingDocCache = new Map<string, Promise<unknown>>();
+const pendingDocCache = new Map<string, Promise<unknown>>()
 
 export async function fetchCached<T>(opts: {
-  fn: () => Promise<T>;
-  key: string;
-  ttl: number;
+  fn: () => Promise<T>
+  key: string
+  ttl: number
 }): Promise<T> {
   if (docCache.has(opts.key)) {
-    return docCache.get(opts.key) as T;
+    return docCache.get(opts.key) as T
   }
 
-  const pending = pendingDocCache.get(opts.key);
+  const pending = pendingDocCache.get(opts.key)
 
   if (pending) {
-    return pending as Promise<T>;
+    return pending as Promise<T>
   }
 
   const resultPromise = opts
@@ -36,15 +36,15 @@ export async function fetchCached<T>(opts: {
     .then((result) => {
       docCache.set(opts.key, result, {
         ttl: opts.ttl,
-      });
+      })
 
-      return result;
+      return result
     })
     .finally(() => {
-      pendingDocCache.delete(opts.key);
-    });
+      pendingDocCache.delete(opts.key)
+    })
 
-  pendingDocCache.set(opts.key, resultPromise);
+  pendingDocCache.set(opts.key, resultPromise)
 
-  return resultPromise;
+  return resultPromise
 }

638f91e4a60cb0363a73e519da2b069d35d87c34
Tanner Linsley <tannerlinsley@gmail.com>
2026-08-02 15:06:19 -0600

    Allow site embed routes in frames

--- src/utils/frame-embedding.ts
+++ src/utils/frame-embedding.ts
@@ -2,6 +2,8 @@ import { isChartsCatalogEmbedPath } from './charts-catalog-embed'
 
 export function isFrameEmbeddingAllowed(pathname: string) {
   return (
+    pathname === '/partners-embed' ||
+    pathname === '/sponsors-embed' ||
     pathname === '/stats/npm/embed' ||
     isChartsCatalogEmbedPath(pathname) ||
     isChartsCatalogEmbedPath(`${pathname}/`)
